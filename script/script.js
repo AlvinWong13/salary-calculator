720bd68feb896db83117a644c2da35fa047b1fdd
Alvin Wong <wong.alvin13@gmail.com>
2021-01-30 17:29:34 -0600

    created function to add employee information to array

--- script/script.js
+++ script/script.js
@@ -1,7 +1,33 @@
 console.log('script.js');
 
-$(document).ready(onready);
+$(document).ready(onReady);
 
+let employee = [];
 function onReady() {
   console.log('Ready Now');
+
+  // Add employee click
+  $(document).on('click', '#inputEmployee', onAddEmployee);
+}
+
+function onAddEmployee(event) {
+  event.preventDefault();
+  console.log('Add Employee');
+
+  // Grab item from DOM
+  let firstName = $('#firstNameInput').val();
+  let lastName = $('#lastNameInput').val();
+  let id = $('#idNumber').val();
+  let job = $('#jobTitle').val();
+  let salary = $('#salaryInput').val();
+
+  // create employee object
+  let employeeInfo = {
+    firstName: firstName,
+    lastName: lastName,
+    id: Number(id),
+    job: job,
+    salary: Number(salary),
+  };
+  console.log('Got Employee', employeeInfo);
 }
